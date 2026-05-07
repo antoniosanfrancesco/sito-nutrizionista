af185fb64883d89b21a1529aeb9b2790ad19ecd1
antoniosanfrancesco <a.sanfra96@gmail.com>
2026-05-07 17:27:51 +0200

    navbar mobile modificata

--- components/TopNavbar.js
+++ components/TopNavbar.js
@@ -1,26 +1,72 @@
 'use client';
 
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useState } from 'react';
+import { Navbar, Nav, Container, Offcanvas } from 'react-bootstrap';
 import Link from 'next/link';
-import { Montserrat } from 'next/font/google';
 
 export default function TopNavBar() {
-	return (
-		<Navbar variant='light' expand="lg" className="shadow-sm sticky-top" style={{backgroundColor: '#f6ede0'}}>
-			<Container fluid>
-				<Navbar.Brand as={Link} href='/' className='brand-responsive fw-bold navbar-brand-a ps-3'>
-				</Navbar.Brand>
+  const [show, setShow] = useState(false);
 
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" className='ms-auto'/>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className='ms-auto pr-3 pt-3 text-end' style={{fontFamily: 'Montserrat'}}>
-						<Nav.Link as={Link} href="/">Home</Nav.Link>
-						<Nav.Link as={Link} href="/chi-sono">Chi Sono</Nav.Link>
-						<Nav.Link as={Link} href="/servizi">Servizi</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  const greenColor = "#1b4d3e";
+
+  return (
+    <Navbar 
+      expand="lg" 
+      className="shadow-sm sticky-top" 
+      style={{ backgroundColor: '#f6ede0', minHeight: '80px' }}
+    >
+      <Container fluid className="px-md-5">
+        {/* Logo/Brand */}
+        <Navbar.Brand as={Link} href='/' className='fw-bold' style={{ fontSize: '1.2rem', color: greenColor }}>
+          Dott.ssa Albina Crapella
+        </Navbar.Brand>
+
+        {/* Tasto Hamburger */}
+        <Navbar.Toggle 
+          aria-controls="offcanvasNavbar" 
+          onClick={handleShow} 
+          className='ms-auto border-0 shadow-none d-lg-none'
+        />
+
+        {/* 1. MENU DESKTOP */}
+        <div className="d-none d-lg-flex ms-auto">
+          <Nav style={{ fontFamily: 'Montserrat' }}>
+            <Nav.Link as={Link} href="/" className="px-3">Home</Nav.Link>
+            {/* Usa href="/#id-sezione" per farlo funzionare da ogni pagina */}
+            <Nav.Link as={Link} href="/chi-sono" className="px-3">Chi Sono</Nav.Link>
+            <Nav.Link as={Link} href="/servizi" className="px-3">Servizi</Nav.Link>
+            <Nav.Link as={Link} href="/#dove-ricevo" className="px-3">Dove Ricevo</Nav.Link>
+          </Nav>
+        </div>
+
+        {/* 2. MENU MOBILE (OFFCANVAS) */}
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          show={show}
+          onHide={handleClose}
+          className="d-lg-none"
+          style={{ backgroundColor: '#f6ede0', width: '280px' }}
+        >
+          <Offcanvas.Header closeButton className="border-bottom">
+            <Offcanvas.Title id="offcanvasNavbarLabel" className="fw-bold" style={{ color: greenColor }}>
+              Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column text-end pe-3" style={{ fontFamily: 'Montserrat' }}>
+              <Nav.Link as={Link} href="/" onClick={handleClose} className="py-3 fs-5">Home</Nav.Link>
+              <Nav.Link as={Link} href="/#chi-sono" onClick={handleClose} className="py-3 fs-5">Chi Sono</Nav.Link>
+              <Nav.Link as={Link} href="/servizi" onClick={handleClose} className="py-3 fs-5">Servizi</Nav.Link>
+              <Nav.Link as={Link} href="/#dove-ricevo" onClick={handleClose} className="py-3 fs-5">Dove Ricevo</Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 }
